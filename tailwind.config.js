be89c0c996deb309baa13fecd57cec1bbfec1db2
guole901-droid <guole901@gmail.com>
2025-12-11 13:23:34 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    // 下面这一行是关键！告诉它扫描当前目录下的所有 jsx 文件
+    "./*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
